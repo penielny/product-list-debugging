cc09f0195fe7d96d1a9ac82dad57bc52f9e345c6
penielny <penielnyinaku@gmail.com>
2025-06-12 18:18:59 +0000

    Enhance dessert card component with dynamic selection state and improve order modal styles for better layout

--- src/app/components/dessert-card/dessert-card.component.ts
+++ src/app/components/dessert-card/dessert-card.component.ts
@@ -1,15 +1,34 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { Dessert } from '../../interfaces/dessert';
 import { AddToCartComponent } from '../add-to-cart/add-to-cart.component';
 import { CommonModule } from '@angular/common';
+import { CartService } from '../../services/cart';
 
 @Component({
   selector: 'app-dessert-card',
-  imports: [AddToCartComponent,CommonModule],
+  imports: [AddToCartComponent, CommonModule],
   templateUrl: './dessert-card.component.html',
   styleUrl: './dessert-card.component.scss'
 })
-export class DessertCardComponent {
-  @Input() dessert!:Dessert;
+export class DessertCardComponent implements OnInit {
+
+  @Input() dessert!: Dessert;
+  isInCart: boolean = false;
+  cartService = inject(CartService)
+
+  ngOnInit(): void {
+    this.cartService.cart$.subscribe({
+      next: () => {
+        const isFound = this.cartService.isInCart(this.dessert.name)
+        if (isFound !== -1) {
+          this.isInCart = true;
+        }else{
+          this.isInCart = false;
+        }
+      }
+    })
+  }
+
+
 
 }
